20f5e815be9903e3642317413368c72a46678ad5
Gradi0us <hieunguyen20032302@gmail.com>
2023-04-04 15:21:29 +0700

    test

--- United_SysChat/screens/profile.js
+++ United_SysChat/screens/profile.js
@@ -7,6 +7,8 @@ const UpdateProfileScreen = () => {
   const [displayName, setDisplayName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [showModal, setShowModal] = useState(false);
+  const [uidExists, setUidExists] = useState(false); // Thêm state để theo dõi xem UID đã có trong database hay chưa
+
   // Lấy thông tin người dùng đăng nhập
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -15,20 +17,30 @@ const UpdateProfileScreen = () => {
     return unsubscribe;
   }, []);
 
-  const updateProfile = async () => {
-    // Kiểm tra xem UID của người dùng đã có đủ thông tin trong database hay chưa
-    const userRef = database.collection('users').doc(auth.currentUser.uid);
-    const userSnapshot = await userRef.get();
+  useEffect(() => {
+    const checkUidExists = async () => {
+      // Kiểm tra xem UID của người dùng đã có đủ thông tin trong database hay chưa
+      const userRef = database.collection('users').doc(auth.currentUser.uid);
+      const userSnapshot = await userRef.get();
 
-    if (userSnapshot.exists && userSnapshot.data().displayName && userSnapshot.data().phoneNumber) {
-      // Nếu đã có đủ thông tin, hiển thị modal để người dùng cập nhật thông tin
-      setDisplayName(userSnapshot.data().displayName);
-      setPhoneNumber(userSnapshot.data().phoneNumber);
-      setShowModal(true);
-    } else {
-      // Nếu chưa đủ thông tin, hiển thị thông báo lỗi
-      alert('Please provide your name and phone number to update your profile.');
+      if (userSnapshot.exists && userSnapshot.data().displayName && userSnapshot.data().phoneNumber) {
+        // Nếu đã có đủ thông tin, lấy thông tin và đánh dấu là đã có trong database
+        setDisplayName(userSnapshot.data().displayName);
+        setPhoneNumber(userSnapshot.data().phoneNumber);
+        setUidExists(true);
+      } else {
+        // Nếu chưa đủ thông tin, đánh dấu là chưa có trong database
+        setUidExists(false);
+      }
+    };
+
+    if (auth.currentUser) {
+      checkUidExists();
     }
+  }, [auth.currentUser]);
+
+  const updateProfile = async () => {
+    setShowModal(true);
   };
 
   const saveProfile = async () => {
@@ -37,21 +49,30 @@ const UpdateProfileScreen = () => {
       displayName,
       phoneNumber,
     });
+
+    // Lấy thông tin người dùng sau khi đã cập nhật
+    const userRef = database.collection('users').doc(auth.currentUser.uid);
+    const userSnapshot = await userRef.get();
+
+    // Cập nhật thông tin hiển thị trên màn hình
+    setDisplayName(userSnapshot.data().displayName);
+    setPhoneNumber(userSnapshot.data().phoneNumber);
+
     // Đóng modal
     setShowModal(false);
   };
+
   return (
     <View>
-      <Text>Display Name:</Text>
-      <TextInput value={displayName} onChangeText={setDisplayName} />
-      <Text>Phone Number:</Text>
-      <TextInput value={phoneNumber} onChangeText={setPhoneNumber} />
+      <Text>UID: {auth.currentUser?.uid}</Text>
+      <Text>Display Name: {displayName} </Text>
+      <Text>Phone Number: {phoneNumber} </Text>
       <Button title="Update Profile" onPress={updateProfile} />
 
       {/* Modal */}
       <Modal visible={showModal}>
         <View>
-          <Text>Update your profile:</Text>
+          <Text>{uidExists ? 'Update your profile:' : 'Please provide your name and phone number to update your profile:'}</Text>
           <Text>Display Name:</Text>
           <TextInput value={displayName} onChangeText={(text) => setDisplayName(text)} />
           <Text>Phone Number:</Text>
@@ -60,7 +81,7 @@ const UpdateProfileScreen = () => {
           <Button title="Cancel" onPress={() => setShowModal(false)} />
         </View>
       </Modal>
-    </View>
+      </View>
   );
 };
 
