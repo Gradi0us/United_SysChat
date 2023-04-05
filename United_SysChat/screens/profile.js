7f575d6c298df444298b81d4084540beb04fbfbc
Gradi0us <hieunguyen20032302@gmail.com>
2023-04-05 14:55:52 +0700

    profileupload success but cannot update if it have the same UID

--- United_SysChat/screens/profile.js
+++ United_SysChat/screens/profile.js
@@ -1,88 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, Modal } from 'react-native';
 import { auth, database } from "../config/firebase";
+import {addDoc, collection } from 'firebase/firestore';
 
 const UpdateProfileScreen = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
   const [displayName, setDisplayName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [avatarUrl, setAvatarurl] = useState('');
+  const [backGrUrl, setBackgroundUrl] = useState('');
   const [showModal, setShowModal] = useState(false);
-  const [uidExists, setUidExists] = useState(false); // Thêm state để theo dõi xem UID đã có trong database hay chưa
-
-  // Lấy thông tin người dùng đăng nhập
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-    });
-    return unsubscribe;
-  }, []);
+  const [uidExists, setUidExists] = useState(false); 
+  const currentUser = auth.currentUser;
+  const usersRef = collection(database, 'users');
 
   useEffect(() => {
-    const checkUidExists = async () => {
-      // Kiểm tra xem UID của người dùng đã có đủ thông tin trong database hay chưa
-      const userRef = database.collection('users').doc(auth.currentUser.uid);
-      const userSnapshot = await userRef.get();
-
-      if (userSnapshot.exists && userSnapshot.data().displayName && userSnapshot.data().phoneNumber) {
-        // Nếu đã có đủ thông tin, lấy thông tin và đánh dấu là đã có trong database
-        setDisplayName(userSnapshot.data().displayName);
-        setPhoneNumber(userSnapshot.data().phoneNumber);
-        setUidExists(true);
-      } else {
-        // Nếu chưa đủ thông tin, đánh dấu là chưa có trong database
-        setUidExists(false);
-      }
-    };
-
-    if (auth.currentUser) {
-      checkUidExists();
+    if (currentUser) {
+      setUser(currentUser);
+      setUidExists(true);
     }
-  }, [auth.currentUser]);
+  }, [currentUser]);
 
-  const updateProfile = async () => {
-    setShowModal(true);
-  };
-
-  const saveProfile = async () => {
-    // Cập nhật thông tin vào database
-    await database.collection('users').doc(auth.currentUser.uid).update({
-      displayName,
-      phoneNumber,
-    });
-
-    // Lấy thông tin người dùng sau khi đã cập nhật
-    const userRef = database.collection('users').doc(auth.currentUser.uid);
-    const userSnapshot = await userRef.get();
-
-    // Cập nhật thông tin hiển thị trên màn hình
-    setDisplayName(userSnapshot.data().displayName);
-    setPhoneNumber(userSnapshot.data().phoneNumber);
-
-    // Đóng modal
-    setShowModal(false);
-  };
+  const handleSave = async () => {
+    try {
+      const userDocRef = collection(database, 'users').doc(user.userID);
+      const doc = await userDocRef.get();
+  
+      if (doc.exists) {
+        // If a document with the same UID already exists, update its fields
+        await userDocRef.update({
+          displayName: displayName,
+          avatarUrl: avatarUrl,
+          backGrUrl: backGrUrl
+        });
+        console.log("Document updated with ID: ", userDocRef.id);
+      } else {
+        // If no document with the same UID exists, create a new document
+        await addDoc(collection(database, 'users'), {
+          uid: user.uid,
+          displayName: displayName,
+          avatarUrl: avatarUrl,
+          backGrUrl: backGrUrl
+        });
+        console.log("New document added with ID: ", userDocRef.id);
+      }
+  
+      setShowModal(false);
+    } catch (e) {
+      console.error("Error saving document: ", e);
+    }
+  }
 
   return (
     <View>
-      <Text>UID: {auth.currentUser?.uid}</Text>
-      <Text>Display Name: {displayName} </Text>
-      <Text>Phone Number: {phoneNumber} </Text>
-      <Button title="Update Profile" onPress={updateProfile} />
-
-      {/* Modal */}
-      <Modal visible={showModal}>
+      <Text>Display Name: {displayName}</Text>
+      <Text>Avatar Url: {avatarUrl}</Text>
+      <Text>Background Url: {backGrUrl}</Text>
+      <Button title="Edit Profile" onPress={() => setShowModal(true)} />
+      <Modal visible={showModal} animationType="slide">
         <View>
-          <Text>{uidExists ? 'Update your profile:' : 'Please provide your name and phone number to update your profile:'}</Text>
-          <Text>Display Name:</Text>
-          <TextInput value={displayName} onChangeText={(text) => setDisplayName(text)} />
-          <Text>Phone Number:</Text>
-          <TextInput value={phoneNumber} onChangeText={(text) => setPhoneNumber(text)} />
-          <Button title="Save" onPress={saveProfile} />
-          <Button title="Cancel" onPress={() => setShowModal(false)} />
+          <TextInput
+            placeholder="Display Name"
+            value={displayName}
+            onChangeText={text => setDisplayName(text)}
+          />
+          <TextInput
+            placeholder="Avatar Url"
+            value={avatarUrl}
+            onChangeText={text => setAvatarurl(text)}
+          />
+          <TextInput
+            placeholder="Background Url"
+            value={backGrUrl}
+            onChangeText={text => setBackgroundUrl(text)}
+          />
+          <Button title="Save" onPress={handleSave} />
         </View>
       </Modal>
-      </View>
+    </View>
   );
-};
-
+}
 export default UpdateProfileScreen;
